77247f861ce50f9859d729af9d8e81dd10d3f613
Mester766 <martin_malet@hotmail.fr>
2018-03-19 11:25:19 +0100

    Merge + voila

--- app/src/main/java/com/example/martin/traitementimage/MainActivity.java
+++ app/src/main/java/com/example/martin/traitementimage/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.martin.traitementimage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivOne,ivTwo;
+    int numImage;
+    TextView tvProgress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ivOne = (ImageView) findViewById(R.id.imageOne);
+        ivTwo = (ImageView) findViewById(R.id.imageTwo);
+        tvProgress = (TextView) findViewById(R.id.tvComputationProgress);
+        numImage = 1;
+        int imageID = getResources().getIdentifier("image"+numImage,"drawable",getPackageName());
+        ivOne.setImageResource(imageID);
+        ivTwo.setImageResource(imageID);
+    }
+
+    protected void changeImage(View v)
+    {
+        numImage++;
+        if ( numImage >= 13 ) numImage = 1;
+        int imageID = getResources().getIdentifier("image"+numImage,"drawable",getPackageName());
+        ivOne.setImageResource(imageID);
+
     }
 }
